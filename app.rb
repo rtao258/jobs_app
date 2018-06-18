7da5866610b1408ba3867a1ece4177572ed8748a
Raymond Tao <rtao@web02.hpc.osc.edu>
2018-06-18 19:23:07 -0400

    help I don't know what's going on sorry

---
+++ app.rb
@@ -0,0 +1,31 @@
+require "sinatra/base"
+require "open3"
+require "nokogiri"
+
+class App < Sinatra::Base
+  set :erb, :escape_html => true
+
+  def title
+    "Jobs App"
+  end
+
+  get "/" do
+    erb :index
+  end
+
+  get "/:user" do
+    @user = params[:user]
+    jobs = []
+    output, status = Open3.capture2e("/opt/torque/bin/qstat -f -x")
+    output = Nokogiri::XML(output)
+    output.xpath("//Job").each do |job|
+      job_owner = job.xpath("//Job_Owner")[0].content.split("@")[0]
+      if job_owner == @user
+        jobs.append(job)
+      end
+    end
+    @output = jobs
+    erb :jobs
+  end
+  
+end
