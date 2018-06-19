f6730c2818863b9814916c6fa3e93e8751e0e761
Raymond Tao <rtao@web02.hpc.osc.edu>
2018-06-19 10:42:45 -0400

    fix most xml parse, some still not working

--- app.rb
+++ app.rb
@@ -10,46 +10,53 @@ class App < Sinatra::Base
   end
 
   get "/" do
-    erb :index
+    erb:index
   end
 
   get "/:user" do
     @user = params[:user]
     jobs = []
+    owners = []
     output, status = Open3.capture2e("/opt/torque/bin/qstat -f -x")
     output = Nokogiri::XML(output)
-    output.xpath("//Job").each do |job|
-      job_owner = job.xpath(//"Job_Owner")[0].content.split("@")[0]
+
+    output.css("Job").each do |job|
+      job_owner = job.at_css("Job_Owner").content.split("@")[0]
+      owners << job_owner
+      
       if job_owner == @user
-        job_name = job.xpath("//Job_Name")[0].content
-        job_ID = job.xpath("//Job_Id")[0].content.split(".")[0]
-        mem = job.xpath("//resources_used")[0].xpath("//mem")[0].content
-        vmem = job.xpath("//resources_used")[0].xpath("//vmem")[0].content
-        walltime = job.xpath("//resources_used")[0].xpath("//walltime")[0].content
         
-        queue = job.xpath("//queue")[0].content
-        server = job.xpath("//server")[0].content
+        job_name = job.at_css("Job_Name").content
+        job_ID = job.at_css("Job_Id").content.split(".")
+        mem = nil #job.at_css("resources_used mem").content
+        vmem = nil #job.at_css("resources_used vmem").content
+        walltime = nil #job.at_css("resources_used walltime").content
+            
+        queue = job.at_css("queue").content
+        server = job.at_css("server").content
         
-        error_path = job.xpath("//Error_Path")[0].content
-        output_path = job.xpath("//Output_Path")[0].content
-        
-        resource_walltime = job.xpath("//Resource_List")[0].xpath("//walltime")[0].content
-        resource_nodes = job.xpath("//Resource_List")[0].xpath("//nodes")[0].content
-        resource_mem = job.xpath("//Resource_List")[0].xpath("//mem")[0].content
-        
-        sesssion_ID = job.xpath("//session_id")[0].content
-        shell_path_list = job.xpath("//Shell_Path_List")[0].content
-        euser = job.xpath("//euser")[0].content
-        egroup = job.xpath("//egroup")[0].content
-        
-        
-        jobs.push({job_owner: job_owner, job_name: job_name, job_ID: job_ID, mem: mem, vmem: vmem, walltime: walltime, queue: queue, server: server, err_path: error_path, out_path: output_path, res_walltime: resource_walltime, res_nodes: resource_nodes, res_mem: resource_mem, ses_ID: session_id, shell_path_list: shell_path_list, euser: euser, egroup: egroup})
-        
-        
+        error_path = job.at_css("Error_Path").content
+        output_path = job.at_css("Output_Path").content
+            
+        resource_walltime = nil #job.at_css("Resource_List walltime").content
+        resource_nodes = nil #job.at_css("Resource_List nodes").content
+        resource_mem = nil #job.at_css("Resource_List mem").content
+            
+        session_ID = nil #job.at_css("session_id").content
+        shell_path_list = job.at_css("Shell_Path_List").content
+        euser = job.at_css("euser").content
+        egroup = job.at_css("egroup").content
+            
+        jobs << {job_owner: job_owner, job_name: job_name, job_ID: job_ID, mem: mem, vmem: vmem, walltime: walltime, 
+        queue: queue, server: server, 
+        err_path: error_path, out_path: output_path, 
+        res_walltime: resource_walltime, res_nodes: resource_nodes, res_mem: resource_mem, 
+        ses_ID: session_ID, shell_path_list: shell_path_list, euser: euser, egroup: egroup}
       end
     end
+    
     @output = jobs
-    erb :jobs
+    erb:jobs
   end
   
 end
